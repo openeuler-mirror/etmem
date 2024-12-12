openeuler-mirror/etmem

commit 012fec939e77e9c362f3b17216d2efb7d9d67647

Adapt PMU sampling function to cslide

etmem/src/etmemd_src/etmemd_cslide.c
@@ -125,6 +125,9 @@ struct cslide_pid_params {
     struct cslide_eng_params *eng_params;
     struct cslide_task_params *task_params;
     struct cslide_pid_params *next;
+#ifdef ENABLE_PMU
+    struct pmu_params *pmu_params;      /* pmu sample params */
+#endif
 };
 
 struct cslide_params_factory {
@@ -151,6 +154,11 @@ struct cslide_eng_params {
     struct cslide_params_factory factory;
     struct node_pages_info *host_pages_info;
     bool finish;
+#ifdef ENABLE_PMU
+    struct pmu_params *pmu_params;
+    int cooling_interval;
+    int cooling_interval_init;
+#endif
 };
 
 struct ctrl_cap {
@@ -1355,6 +1363,93 @@ static int cslide_do_scan(struct cslide_eng_params *eng_params)
     return 0;
 }
 
+#ifdef ENABLE_PMU
+static int cslide_do_sample(struct cslide_eng_params *eng_params)
+{
+    struct cslide_pid_params *each_pid_params = NULL;
+    int ret;
+
+    factory_foreach_working_pid_params(each_pid_params, &eng_params->factory) {
+        if (cslide_get_vmas(each_pid_params) != 0) {
+            etmemd_log(ETMEMD_LOG_ERR, "cslide get vmas fail\n");
+            return -1;
+        }
+    }
+
+    etmemd_log(ETMEMD_LOG_DEBUG, "cslide pmu : read pmu_params to sample");
+    factory_foreach_working_pid_params(each_pid_params, &eng_params->factory) {
+        if (each_pid_params->pmu_params == NULL) {
+            each_pid_params->pmu_params = calloc(1, sizeof(struct pmu_params));
+            if (each_pid_params->pmu_params == NULL) {
+                etmemd_log(ETMEMD_LOG_ERR, "Memory allocation for pmu_params failed.\n");
+                return -1;
+            }
+        }
+
+        each_pid_params->pmu_params = eng_params->pmu_params;
+        each_pid_params->pmu_params->pid = each_pid_params->pid;
+        
+        ret = pthread_mutex_init(&(each_pid_params->pmu_params->vma_list_mutex), NULL);
+        if (ret != 0) {
+            etmemd_log(ETMEMD_LOG_ERR, "init vma_list_mutex failed.\n");
+            return -1;
+        }
+
+        etmemd_do_sample(each_pid_params->pmu_params);
+    }
+
+    return 0;
+}
+
+/**
+ * cooling - Perform cooling by updating page reference counts
+ *
+ * This function updates the page reference counts by aggregating
+ * data from multiple `count_page_refs` entries. The aggregation
+ * is performed by directly dividing the index by a smoothing factor
+ * (in this case, 2) to achieve a form of averaging.
+ */
+static void cooling(struct cslide_pid_params *pid_params)
+{
+    struct count_page_refs *cpfs = pid_params->count_page_refs;
+    int count = pid_params->count;
+    /* 2 is used for calculating the exponential moving average of access frequency */
+    int smoothing = 2;
+
+    for (int c = 1; c < count; c++) {
+        struct count_page_refs *src_cpf = &cpfs[c];
+        struct count_page_refs *dest_cpf = &cpfs[c / smoothing];
+
+        for (int n = 0; n < src_cpf->node_num; n++) {
+            struct node_page_refs *src_node = &src_cpf->node_pfs[n];
+            struct node_page_refs *dest_node = &dest_cpf->node_pfs[n];
+
+            if (src_node->head == NULL) {
+                continue;
+            }
+
+            if (dest_node->tail) {
+                dest_node->tail->next = src_node->head;
+            } else {
+                dest_node->head = src_node->head;
+            }
+            dest_node->tail = src_node->tail;
+            dest_node->num += src_node->num;
+
+            struct page_refs *current = src_node->head;
+            while (current) {
+                current->count = c / smoothing;
+                current = current->next;
+            }
+
+            src_node->head = NULL;
+            src_node->tail = NULL;
+            src_node->num = 0;
+        }
+    }
+}
+#endif
+
 // error return -1; success return moved pages number
 static int do_migrate_pages(unsigned int pid, struct page_refs *page_refs, int node)
 {
@@ -1508,6 +1603,13 @@ static void update_pages_info(struct cslide_eng_params *eng_params, struct cslid
         host_pages[n].cold += task_pages[n].cold;
         host_pages[n].hot += task_pages[n].hot;
     }
+#ifdef ENABLE_PMU
+    eng_params->cooling_interval -= pid_params->pmu_params->vma_updata_count;
+    if (eng_params->cooling_interval < 0) {
+        cooling(pid_params);
+        eng_params->cooling_interval = eng_params->cooling_interval_init;
+    }
+#endif
 }
 
 static void cslide_stat(struct cslide_eng_params *eng_params)
@@ -1558,6 +1660,10 @@ static void destroy_cslide_eng_params(struct cslide_eng_params *params)
 {
     free(params->host_pages_info);
     params->host_pages_info = NULL;
+#ifdef ENABLE_PMU
+    free(params->pmu_params);
+    params->pmu_params = NULL;
+#endif
     destroy_factory(&params->factory);
     pthread_mutex_destroy(&params->stat_mtx);
     destroy_node_map(&params->node_map);
@@ -1611,10 +1717,46 @@ destroy_sys_mem:
     return -1;
 }
 
+static void cleanup_and_destroy(struct cslide_eng_params *eng_params)
+{
+    factory_update_pid_params(&eng_params->factory);
+    destroy_cslide_eng_params(eng_params);
+    free(eng_params);
+}
+
+static int perform_memory_operations(struct cslide_eng_params *eng_params)
+{
+#ifdef ENABLE_PMU
+    if (eng_params->pmu_params->sample_period == 0) {
+        struct sys_mem *mem = NULL;
+        mem = &eng_params->mem;
+        if (get_sys_mem(mem) != 0) {
+            return -1;
+        }
+        if (cslide_do_scan(eng_params) != 0) {
+            return -1;
+        }
+    } else if (cslide_do_sample(eng_params) != 0) {
+        return -1;
+    }
+#else
+    struct sys_mem *mem = NULL;
+    mem = &eng_params->mem;
+    if (get_sys_mem(mem) != 0) {
+        etmemd_log(ETMEMD_LOG_ERR, "get system meminfo fail\n");
+        return -1;
+    }
+    if (cslide_do_scan(eng_params) != 0) {
+        etmemd_log(ETMEMD_LOG_ERR, "cslide_do_scan fail\n");
+        return -1;
+    }
+#endif
+    return 0;
+}
+
 static void *cslide_main(void *arg)
 {
     struct cslide_eng_params *eng_params = (struct cslide_eng_params *)arg;
-    struct sys_mem *mem = NULL;
 
     // only invalid pthread id or deatch more than once will cause error
     // so no need to check return value of pthread_detach
@@ -1630,14 +1772,7 @@ static void *cslide_main(void *arg)
             goto next;
         }
 
-        mem = &eng_params->mem;
-        if (get_sys_mem(mem) != 0) {
-            etmemd_log(ETMEMD_LOG_ERR, "get system meminfo fail\n");
-            goto next;
-        }
-
-        if (cslide_do_scan(eng_params) != 0) {
-            etmemd_log(ETMEMD_LOG_ERR, "cslide_do_scan fail\n");
+        if (perform_memory_operations(eng_params) != 0) {
             goto next;
         }
 
@@ -1656,9 +1791,7 @@ next:
         cslide_clean_params(eng_params);
     }
 
-    factory_update_pid_params(&eng_params->factory);
-    destroy_cslide_eng_params(eng_params);
-    free(eng_params);
+    cleanup_and_destroy(eng_params);
     return NULL;
 }
 
@@ -2065,11 +2198,110 @@ static int fill_mig_quota(void *obj, void *val)
     return 0;
 }
 
+#ifdef ENABLE_PMU
+static int init_pmu_params(struct cslide_eng_params *params)
+{
+    if (params->pmu_params == NULL) {
+        params->pmu_params = calloc(1, sizeof(struct pmu_params));
+        if (params->pmu_params == NULL) {
+            etmemd_log(ETMEMD_LOG_ERR, "Memory allocation for pmu_params failed.\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int fill_vma_updata_rate(void *obj, void *val)
+{
+    if (params->pmu_params == NULL) {
+        params->pmu_params = calloc(1, sizeof(struct pmu_params));
+        if (params->pmu_params == NULL) {
+            etmemd_log(ETMEMD_LOG_ERR, "Memory allocation for pmu_params failed.\n");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+static int fill_vma_updata_rate(void *obj, void *val)
+{
+    struct cslide_eng_params *params = (struct cslide_eng_params *)obj;
+    int value = parse_to_int(val);
+    if (value < 0) {
+        etmemd_log(ETMEMD_LOG_ERR, "config vma_updata_rate %d not valid\n", value);
+        return -1;
+    }
+    if (init_pmu_params(params) < 0) {
+        return -1;
+    }
+
+    params->pmu_params->vma_updata_rate = value;
+    params->pmu_params->vma_updata_count = 0;
+    return 0;
+}
+
+static int fill_sample_period(void *obj, void *val)
+{
+    struct cslide_eng_params *params = (struct cslide_eng_params *)obj;
+    int value = parse_to_int(val);
+    if (value < 0) {
+        etmemd_log(ETMEMD_LOG_ERR, "config sample_period %d not valid\n", value);
+        return -1;
+    }
+    if (init_pmu_params(params) < 0) {
+        return -1;
+    }
+	
+    params->pmu_params->sample_period = value;
+    return 0;
+}
+
+static int fill_cpu_set_size(void *obj, void *val)
+{
+    struct cslide_eng_params *params = (struct cslide_eng_params *)obj;
+    int value = parse_to_int(val);
+    if (value < 0) {
+        etmemd_log(ETMEMD_LOG_ERR, "config cpu_set_size %d not valid\n", value);
+        return -1;
+    }
+    if (init_pmu_params(params) < 0) {
+        return -1;
+    }
+	
+    params->pmu_params->cpu_set_size = value;
+    params->pmu_params->swap_flag = 0;
+    return 0;
+}
+
+static int fill_cooling_interval(void *obj, void *val)
+{
+    struct cslide_eng_params *params = (struct cslide_eng_params *)obj;
+    int value = parse_to_int(val);
+    if (value < 0) {
+        etmemd_log(ETMEMD_LOG_ERR, "config cooling_interval %d not valid\n", value);
+        return -1;
+    }
+    if (init_pmu_params(params) < 0) {
+        return -1;
+    }
+	
+    params->cooling_interval = value;
+    params->cooling_interval_init = value;
+    return 0;
+}
+#endif
+
 static struct config_item cslide_eng_config_items[] = {
     {"node_pair", STR_VAL, fill_node_pair, false},
     {"hot_threshold", INT_VAL, fill_hot_threshold, false},
     {"node_mig_quota", INT_VAL, fill_mig_quota, false},
     {"node_hot_reserve", INT_VAL, fill_hot_reserve, false},
+#ifdef ENABLE_PMU
+    {"sample_period", INT_VAL, fill_sample_period, false},
+    {"vma_updata_rate", INT_VAL, fill_vma_updata_rate, false},
+    {"cpu_set_size", INT_VAL, fill_cpu_set_size, false},
+    {"cooling_interval", INT_VAL, fill_cooling_interval, false},
+#endif
 };
 
 static int cslide_fill_eng(GKeyFile *config, struct engine *eng)

etmem/src/etmemd_src/etmemd_slide.c
@@ -211,6 +211,7 @@ static void *slide_executor(void *arg)
     struct page_refs *page_refs = NULL;
     struct memory_grade *memory_grade = NULL;
     struct page_sort *page_sort = NULL;
+    struct task *task_ptr = (struct task *)tk_pid->tk;
 
     /* The pthread_setcancelstate interface returns an error only when the
      * input parameter state is invalid, no need to check return value.
@@ -225,13 +226,16 @@ static void *slide_executor(void *arg)
     }
 
 #ifdef ENABLE_PMU
-    if (((struct slide_params *)tk_pid->tk->params)->pmu_params == NULL) {
-        page_refs = etmemd_do_scan(tk_pid, tk_pid->tk);
+    struct slide_params *slide = (struct slide_params *)task_ptr->params;
+    if ((slide->pmu_params == NULL) {
+        page_refs = etmemd_do_scan(tk_pid, task_ptr);
     } else {
-        page_refs = etmemd_do_sample(tk_pid, tk_pid->tk);
+        slide->pmu_params->pid = tk_pid->pid;
+        slide->pmu_params->swap_flag = task_ptr->swap_flag;
+        page_refs = etmemd_do_sample(slide->pmu_params);
     }
 #else
-    page_refs = etmemd_do_scan(tk_pid, tk_pid->tk);
+    page_refs = etmemd_do_scan(tk_pid, task_ptr);
 #endif
     if (page_refs == NULL) {
         etmemd_log(ETMEMD_LOG_WARN, "pid %u cannot get page refs\n", tk_pid->pid);
@@ -268,8 +272,9 @@ scan_out:
 
 exit:
 #ifdef ENABLE_PMU
-    if (((struct slide_params *)tk_pid->tk->params)->pmu_params != NULL) {
-        merge_page_refs(tk_pid, &page_sort, &memory_grade);
+    if (((struct slide_params *)task_ptr->params)->pmu_params != NULL) {
+        merge_page_refs(((struct slide_params *)task_ptr->params)->pmu_params,
+            &page_sort, &memory_grade);
     }
 #endif
     clean_memory_grade_unexpected(&memory_grade);
@@ -483,7 +488,7 @@ static void slide_stop_task(struct engine *eng, struct task *tk)
 
 #ifdef ENABLE_PMU
     if (params->pmu_params != NULL) {
-        etmemd_stop_sample(tk);
+        etmemd_stop_sample(params->pmu_params);
     }
 #endif
 

etmem/conf/cslide_conf.yaml
@@ -13,6 +13,10 @@ node_pair=0,2;1,3
 hot_threshold=1
 node_mig_quota=1024
 node_hot_reserve=1024
+sample_period=5000
+vma_updata_rate=3
+cpu_set_size=12
+cooling_interval=100000
 
 [task]
 project=test

etmem/inc/etmemd_inc/etmemd_scan.h
@@ -78,6 +78,18 @@ struct walk_address {
 struct page_refs *etmemd_do_scan(const struct task_pid *tpid, const struct task *tk);
 
 #ifdef ENABLE_PMU
+struct pmu_params {
+    uint64_t sample_period;         /* Sampling mem access events every N instructions */
+    uint32_t vma_updata_rate;       /* Update after every N slide migrations */
+    uint32_t cpu_set_size;          /* Number of CPU cores sampled by one thread */
+    struct vma_info *vma_list;      /* Pointer to a linked list of VMA (Virtual Memory Area) information */
+    struct page_refs *page_refs_head;             /* Pointer to the head of the page_refs linked list */
+    struct sample_thread_meta *threads_meta_set;  /* Pointer to a set of sampled thread metadata */
+    int vma_updata_count;                         /* Counter for VMA update operations */
+    pthread_mutex_t vma_list_mutex;               /* Mutex for protecting access to the VMA list */
+    unsigned int pid;
+    int swap_flag;
+};
 #define BITS_IN_INT (sizeof(int) * CHAR_BIT) // get the number of bits in an int
 /* Assume the hardware events count following a power-law distribution */
 static inline int limit_count_to_loop(int count, int loop)
@@ -85,19 +97,19 @@ static inline int limit_count_to_loop(int count, int loop)
     int log2 = (int)(BITS_IN_INT - __builtin_clz(count + 1));
     return loop < log2 ? loop : log2;
 }
-void merge_page_refs(struct task_pid *tpid, struct page_sort **page_sort, struct memory_grade **memory_grade);
+void merge_page_refs(struct pmu_params *pmu_params, struct page_sort **page_sort, struct memory_grade **memory_grade);
 
 /* get the page_ref list from the buffer, if the pmu_sample thread has not been started, start the thread. */
-struct page_refs *etmemd_do_sample(struct task_pid *tpid, const struct task *tk);
+struct page_refs *etmemd_do_sample(struct pmu_params *pmu_params);
 
 /* stop pmu_sample thread when stopping the task. */
-void etmemd_stop_sample(struct task *tk);
+void etmemd_stop_sample(struct pmu_params *pmu_params);
 #else
-inline struct page_refs *etmemd_do_sample(struct task_pid *tpid, const struct task *tk)
+inline struct page_refs *etmemd_do_sample(struct pmu_params *pmu_params)
 {
     return NULL;
 }
-inline void etmemd_stop_sample(struct task *tk)
+inline void etmemd_stop_sample(struct pmu_params *pmu_params)
 {
 }
 #endif

etmem/inc/etmemd_inc/etmemd_slide.h
@@ -18,19 +18,7 @@
 
 #include "etmemd_pool_adapter.h"
 #include "etmemd_engine.h"
-
-#ifdef ENABLE_PMU
-struct pmu_params {
-    uint64_t sample_period;  /* Sampling mem access events every N instructions */
-    uint32_t vma_updata_rate;  /* Update after every N slide migrations */
-    uint32_t cpu_set_size;    /* Number of CPU cores sampled by one thread */
-    struct vma_info *vma_list;  /* Pointer to a linked list of VMA (Virtual Memory Area) information */
-    struct page_refs *page_refs_head;  /* Pointer to the head of the page_refs linked list */
-    struct sample_thread_meta *threads_meta_set;  /* Pointer to a set of sampled thread metadata */
-    int vma_updata_count;  /* Counter for VMA update operations */
-    pthread_mutex_t vma_list_mutex;  /* Mutex for protecting access to the VMA list */
-};
-#endif
+#include "etmemd_scan.h"
 
 struct slide_params {
     struct task_executor *executor;

commit b21f2c49b2e53ca58b2fd40f7aff582887be76d6

etmem: Fix two minor issues that prevented compilation when PMU is enabled
I ran into the following issues when building with -DENABLE_PMU=ON option,

---------------------------------------------------------------------
[ 14%] Building C object etmem/CMakeFiles/etmemd.dir/src/etmemd_src/etmemd_slide.c.o
/root/dev/etmem/etmem/src/etmemd_src/etmemd_slide.c: In function ‘slide_executor’:
/root/dev/etmem/etmem/src/etmemd_src/etmemd_slide.c:230:36: error: expected ‘)’ before ‘{’ token
  230 |     if ((slide->pmu_params == NULL) {
      |        ~                           ^~
      |                                    )
---------------------------------------------------------------------

removing the extra parentheses solves the problem.  After fixing it, I found
another compilation error,

---------------------------------------------------------------------
[  3%] Building C object etmem/CMakeFiles/etmemd.dir/src/etmemd_src/etmemd_cslide.c.o
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c: In function ‘fill_vma_updata_rate’:
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c:2216:9: error: ‘params’ undeclared (first use in this function); did you mean ‘pmu_params’?
 2216 |     if (params->pmu_params == NULL) {
      |         ^~~~~~
      |         pmu_params
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c:2216:9: note: each undeclared identifier is reported only once for each function it appears in
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c: At top level:
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c:2226:12: error: redefinition of ‘fill_vma_updata_rate’
 2226 | static int fill_vma_updata_rate(void *obj, void *val)
      |            ^~~~~~~~~~~~~~~~~~~~
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c:2214:12: note: previous definition of ‘fill_vma_updata_rate’ with type ‘int(void *, void *)’
 2214 | static int fill_vma_updata_rate(void *obj, void *val)
      |            ^~~~~~~~~~~~~~~~~~~~
/root/dev/etmem/etmem/src/etmemd_src/etmemd_cslide.c:2214:12: error: ‘fill_vma_updata_rate’ defined but not used [-Werror=unused-function]
cc1: all warnings being treated as errors
---------------------------------------------------------------------

The root cause of this reported error is the redefined fill_vma_updata_rate
function, and this patch removes the redundant one.

After submitting this patch, the openeuler-ci-bot pointed out that the
slide_executor function was too long.  Therefore, this patch also reduces
the length of the function.

Tested by rebuilding on x86_64 openEuler 20.03, 22.03 and 24.03.

Fixes: 012fec939e77 ("Adapt PMU sampling function to cslide")

etmem/src/etmemd_src/etmemd_cslide.c
@@ -2213,18 +2213,6 @@ static int init_pmu_params(struct cslide_eng_params *params)
 
 static int fill_vma_updata_rate(void *obj, void *val)
 {
-    if (params->pmu_params == NULL) {
-        params->pmu_params = calloc(1, sizeof(struct pmu_params));
-        if (params->pmu_params == NULL) {
-            etmemd_log(ETMEMD_LOG_ERR, "Memory allocation for pmu_params failed.\n");
-            return -1;
-        }
-    }
-    return 0;
-}
-
-static int fill_vma_updata_rate(void *obj, void *val)
-{
     struct cslide_eng_params *params = (struct cslide_eng_params *)obj;
     int value = parse_to_int(val);
     if (value < 0) {

etmem/src/etmemd_src/etmemd_slide.c
@@ -205,6 +205,28 @@ static int check_should_swap(struct task_pid *tk_pid)
     return check_pidmem_lower_threshold(tk_pid);
 }
 
+#ifdef ENABLE_PMU
+static struct page_refs *handle_page_refs(struct task_pid *tk_pid, struct task *task_ptr)
+{
+    struct slide_params *slide = (struct slide_params *)task_ptr->params;
+    struct page_refs *page_refs = NULL;
+    if (slide->pmu_params == NULL) {
+        page_refs = etmemd_do_scan(tk_pid, task_ptr);
+    } else {
+        slide->pmu_params->pid = tk_pid->pid;
+        slide->pmu_params->swap_flag = task_ptr->swap_flag;
+        page_refs = etmemd_do_sample(slide->pmu_params);
+    }
+    return page_refs;
+}
+#else
+static struct page_refs *handle_page_refs(struct task_pid *tk_pid, struct task *task_ptr)
+{
+    return etmemd_do_scan(tk_pid, task_ptr);
+}
+#endif
+
+
 static void *slide_executor(void *arg)
 {
     struct task_pid *tk_pid = (struct task_pid *)arg;
@@ -225,18 +247,7 @@ static void *slide_executor(void *arg)
         goto scan_out;
     }
 
-#ifdef ENABLE_PMU
-    struct slide_params *slide = (struct slide_params *)task_ptr->params;
-    if ((slide->pmu_params == NULL) {
-        page_refs = etmemd_do_scan(tk_pid, task_ptr);
-    } else {
-        slide->pmu_params->pid = tk_pid->pid;
-        slide->pmu_params->swap_flag = task_ptr->swap_flag;
-        page_refs = etmemd_do_sample(slide->pmu_params);
-    }
-#else
-    page_refs = etmemd_do_scan(tk_pid, task_ptr);
-#endif
+    page_refs = handle_page_refs(tk_pid, task_ptr);
     if (page_refs == NULL) {
         etmemd_log(ETMEMD_LOG_WARN, "pid %u cannot get page refs\n", tk_pid->pid);
         goto scan_out;